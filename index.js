9cbb4d09760884ec343d7dc8b7e1813652f2418b
vrde <agranzot@gmail.com>
2019-01-24 10:03:44 +0100

    update to new metamask privacy feature

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
-(function (name) {
-	function resolveWeb3 (resolve, localProvider) {
-		let { web3 } = window
-		const alreadyInjected = typeof web3 !== 'undefined'
+(function(name) {
+  function resolveWeb3(resolve, localProvider, authentication) {
+    let web3;
 
-		if (alreadyInjected) {
-			console.log('Injected web3 detected.')
-			web3 = new Web3(web3.currentProvider)
-		} else {
-			console.log('No web3 instance injected, using Local web3.')
-			const provider = new Web3.providers.HttpProvider(localProvider)
-			web3 = new Web3(provider)
-		}
+    if (window.ethereum) {
+      web3 = new Web3(window.ethereum);
+      if (authentication === true) {
+        try {
+          window.ethereum.enable().then(() => resolve(web3));
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        resolve(web3);
+      }
+    } else if (window.web3) {
+      web3 = new Web3(window.web3.currentProvider);
+      resolve(web3);
+    } else {
+      if (authentication === true) {
+        throw new Error(
+          "Non-Ethereum browser detected. Cannot work in authenticated mode"
+        );
+      }
+      web3 = new Web3(localProvider);
+      console.log(`Non-Ethereum browser detected. Using ${localProvider}.`);
+      resolve(web3);
+    }
+  }
 
-		resolve(web3)
-	}
+  function _getWeb3(localProvider, authentication) {
+    localProvider = localProvider || "http://localhost:8545";
 
-	function loadWeb3 (localProvider) {
-		if (localProvider === undefined) {
-			localProvider = 'http://localhost:9545'
-		}
+    return new Promise(resolve => {
+      // Wait for loading completion to avoid race conditions with web3 injection timing.
+      window.addEventListener("load", () => {
+        resolveWeb3(resolve, localProvider, authentication);
+      });
+      // If document has loaded already, try to get Web3 immediately.
+      if (document.readyState === "complete") {
+        resolveWeb3(resolve, localProvider, authentication);
+      }
+    });
+  }
 
-		return new Promise((resolve) => {
-			// Wait for loading completion to avoid race conditions with web3 injection timing.
-			window.addEventListener('load', () => {
-				resolveWeb3(resolve, localProvider)
-			})
-			// If document has loaded already, try to get Web3 immediately.
-			if (document.readyState === 'complete') {
-				resolveWeb3(resolve, localProvider)
-			}
-		})
-	}
+  function loadWeb3(localProvider) {
+    return _getWeb3(localProvider, true);
+  }
 
-	window[name] = loadWeb3
-})('loadWeb3')
+  window[name] = loadWeb3;
+})("loadWeb3");
